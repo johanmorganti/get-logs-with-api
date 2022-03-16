2382a3c4389e01c2a90adc6a2c8386bd657d9867
Johan Morganti <johan.morganti@datadoghq.com>
2022-03-16 12:35:58 +0900

    first poc working

---
+++ README.md
@@ -0,0 +1,21 @@
+# Goals
+
+This code will help customers to download their logs currently being indexed on Datadog platform.
+Leveraging Datadog API : https://docs.datadoghq.com/api/latest/logs/#search-logs
+And pagination : https://docs.datadoghq.com/logs/guide/collect-multiple-logs-with-pagination/?tab=v1api
+
+# Installation
+
+1. Clone the repo
+2. Install dependencies :
+```
+pip install -r requirements.txt
+```
+
+3. Set up env vars DD_API_KEY and DD_APP_KEY
+    * Note : DD_SITE is optional, defaults to `datadoghq.com`
+* Or remove the commented part of the code
+```
+configuration.api_key["apiKeyAuth"] = "*********"
+configuration.api_key["appKeyAuth"] = "*********"
+```
---
+++ get-logs.py
@@ -0,0 +1,48 @@
+import json
+from dateutil.parser import parse as dateutil_parser
+
+from datadog_api_client.v2 import ApiClient, ApiException, Configuration
+from datadog_api_client.v2.api.logs_api import LogsApi
+from datadog_api_client.v2.model.logs_list_request import LogsListRequest
+from datadog_api_client.v2.model.logs_list_request_page import LogsListRequestPage
+from datadog_api_client.v2.model.logs_query_filter import LogsQueryFilter
+from datadog_api_client.v2.model.logs_sort import LogsSort
+
+from pprint import pprint
+
+
+# configuration.api_key["apiKeyAuth"] = "*********"
+# configuration.api_key["appKeyAuth"] = "*********"
+
+configuration = Configuration()
+
+log_filename = ""
+
+with ApiClient(configuration) as api_client:
+    # Create an instance of the API class
+    api_instance = LogsApi(api_client)
+    filter_query = "*"  # str | Search query following logs syntax. (optional)
+    filter_index = "main"  # str | For customers with multiple indexes, the indexes to search Defaults to '*' which means all indexes (optional)
+    filter_from = dateutil_parser('2022-03-08T11:48:36+01:00')  # datetime | Minimum timestamp for requested logs. (optional)
+    filter_to = dateutil_parser('2022-03-08T15:55:36+01:00')  # datetime | Maximum timestamp for requested logs. (optional)
+    sort = LogsSort("timestamp")  # LogsSort | Order of logs in results. (optional)
+    page_limit = 10  # int | Maximum number of logs in the response. (optional) if omitted the server will use the default value of 10
+
+    try:        
+        api_response = api_instance.list_logs_get(filter_query=filter_query, filter_index=filter_index, filter_from=filter_from, filter_to=filter_to, sort=sort, page_limit=page_limit)
+        log_filename = str(filter_from) + "-" + str(filter_to) + ".json"
+        f = open("data/" + log_filename, "a")
+        f.write(str(api_response["data"]))
+        f.close()
+
+        while "meta" in api_response:
+            i = 0
+            api_response = api_instance.list_logs_get(filter_query=filter_query, filter_index=filter_index, filter_from=filter_from, filter_to=filter_to, sort=sort, page_cursor=api_response["meta"]["page"]["after"], page_limit=page_limit)
+            log_filename = str(filter_from) + "-" + str(filter_to) + "-" + str(i) + ".json"
+            f = open("data/" + log_filename, "a")
+            f.write(str(api_response["data"]))
+            f.close()
+            i = i+1
+
+    except ApiException as e:
+        print("Exception when calling LogsApi->list_logs_get: %s\n" % e)
